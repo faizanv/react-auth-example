049863d20019847a1ca760c6ab5ab381762df6b8
Faizan Virani <fvirani@lyft.com>
2019-01-12 22:27:27 -0800

    Updtaed middleware

--- middleware.js
+++ middleware.js
@@ -15,7 +15,7 @@ const withAuth = function(req, res, next) {
       if (err) {
         res.status(401).send('Unauthorized: Invalid token');
       } else {
-        res.email = decoded.email;
+        req.email = decoded.email;
         next();
       }
     });
